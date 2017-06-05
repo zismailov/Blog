1ae9e75c5ee7f361183ed63c840ac22ac3ae9f16
zulkarnay <bedelbaev@gmail.com>
2017-06-05 15:19:27 +0600

    set up database

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    
+
+    name "Test User"
+    email "test@example.com"
+    password "password123"
+
+    trait :admin do
+      role 'admin'
+    end
+
   end
 end
