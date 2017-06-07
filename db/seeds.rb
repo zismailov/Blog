98b180aa5abae3cfcc52be37a6acda38358b79eb
zulkarnay <bedelbaev@gmail.com>
2017-06-07 13:32:50 +0600

    add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,21 @@ User.destroy_all
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
-User.create(email:'Adamsmith@adamsmith.by', password: 'adamsmith', password_confirmation: 'adamsmith', confirmed_at: 5.days.ago)
+# Users
+xyu = User.create(email:'Xyimba@xyu.by', password: 'xyu', password_confirmation: 'xyu', confirmed_at: 1.days.ago)
+potato= User.create(email:'Potato@gmail.com', password: 'potato', password_confirmation: 'potato', confirmed_at: 2.days.ago, created_at: 3.days.ago)
+# Posts
+post1 = Post.create(user_id: rosen.id, title: 'Puta', content: 'Te quero puta! Mass Mass Mass por favor. Mass Mass Mass si si senior')
+post2 = Post.create(user_id: rosen.id, title: 'Home sweet home!', content: 'Home sweet home. It\'s the place where you can hide from your problems.')
+post3 = Post.create(user_id: rosen.id, title: 'JS ensherinka', content: 'Ensherinka tryed to learn js, he understand something, but can understand all what he needed.')
+post4 = Post.create(user_id: rosen.id, title: 'KFC Rat-ta-ta!', content: 'Rat was founded in KFC stipces, bon appetit!Te')
+# Comments
+Comment.create(user_id: rosen.id, post_id: post1.id, content: 'Beautyfull!')
+Comment.create(user_id: potato.id, post_id: post1.id, content: 'Epic! Just epic!!!')
+Comment.create(user_id: rosen.id, post_id: post2.id, content: 'Das ist mein teil...')
+Comment.create(user_id: potato.id, post_id: post2.id, content: 'Epic! Just epic!!!')
+Comment.create(user_id: potato.id, post_id: post2.id, content: '@#!@#@!#@!#@!#@!#@#@!@!#@!#')
+Comment.create(user_id: rosen.id, post_id: post3.id, content: 'Unbelievable!')
+Comment.create(user_id: potato.id, post_id: post3.id, content: 'Unbelievable!')
+Comment.create(user_id: potato.id, post_id: post4.id, content: 'Unbelievable!')
+Comment.create(user_id: potato.id, post_id: post4.id, content: 'Sh*t material. Unfollow!')
