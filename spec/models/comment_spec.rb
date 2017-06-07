27c2e73a9c3286a00bbb9d9b6750a0bf8b3c0a1f
zulkarnay <bedelbaev@gmail.com>
2017-06-07 12:54:43 +0600

    improve comment tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,4 +2,12 @@ describe Comment do
   it 'has a valid factory' do
     expect(build(:comment)).to be_valid
   end
+
+  it 'invalid without a content' do
+    expect(build(:comment, content: nil)).to_not be_valid
+  end
+
+  it 'invalid with blank content' do
+    expect(build(:comment, content: '')).to_not be_valid
+  end
 end
