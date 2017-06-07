8b5188d3608f6064e544f31c0ea098996d5d1bbb
zulkarnay <bedelbaev@gmail.com>
2017-06-07 14:14:20 +0600

    add edit post spec

---
+++ spec/features/posts/edit_posts_spec.rb
@@ -0,0 +1,10 @@
+feature 'Edit post' do
+  scenario 'user can edit post' do
+  end
+
+  scenario 'user can\'t edit another\'s posts' do
+  end
+
+  scenario 'visitor can\'t edit post' do
+  end
+end
